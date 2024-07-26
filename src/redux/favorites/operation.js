d6f4c1de53a8fb2537f60ff518514712785b32ae
Sw1tlana <127736251+Sw1tlana@users.noreply.github.com>
2024-07-26 11:56:43 +0300

    Changrs getCamper mockapi

--- src/redux/favorites/operation.js
+++ src/redux/favorites/operation.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export const getCamper = createAsyncThunk("camper/fetchAll",
     async (_, thunkAPI) => {
         try {
-            const response = await axios.get('https://6698cac22069c438cd700783.mockapi.io/adverts');
+            const response = await axios.get('https://669ce4de15704bb0e3048ae2.mockapi.io/adverts');
             return response.data;
         } catch (error) {
             return thunkAPI.rejectWithValue(error.message);
